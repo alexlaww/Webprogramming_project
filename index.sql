61b10b606c75258559feab667a735a880ae200b3
alexlawtengyi <alex_l.t.y@hotmail.co.uk>
2024-11-06 10:40:58 +0800

    UPDATING
    UPDATE NOT NULL

--- index.sql
+++ index.sql
@@ -2,9 +2,11 @@ create database student_information;
 use student_information;
 
 create table student(
-Student_id int primary key,
+Student_id int NOT NULL AUTO_INCREMENT,
 Student_name varchar(40),
 Student_email varchar(50),
 Contact_Num varchar(20),
-Username varchar(40)
+Username varchar(40),
+
+PRIMARY KEY(Student_id)
 )
