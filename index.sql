98b10c255d87b17a962305504ed60c3b91d2165b
alexlawtengyi <alex_l.t.y@hotmail.co.uk>
2024-11-06 10:35:43 +0800

    create sql
    database student_information

---
+++ index.sql
@@ -0,0 +1,10 @@
+create database student_information;
+use student_information;
+
+create table student(
+Student_id int primary key,
+Student_name varchar(40),
+Student_email varchar(50),
+Contact_Num varchar(20),
+Username varchar(40)
+)
